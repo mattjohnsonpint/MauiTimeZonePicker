f55ba8a5f9184ba66411c0c9398419b8720c30f1
Matt Johnson-Pint <matt.johnson-pint@sentry.io>
2022-07-18 12:48:48 -0700

    Fix errors

---
+++ src/TimeZoneResourceProvider.Other.cs
@@ -0,0 +1,14 @@
+namespace MauiTimeZonePicker;
+
+public class TimeZoneResourceProvider : ITimeZoneResourceProvider
+{
+    public string GetGenericName(string timeZoneId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string GetLocation(string timeZoneId)
+    {
+        throw new NotImplementedException();
+    }
+}
