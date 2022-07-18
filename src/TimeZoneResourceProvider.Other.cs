cf17f414647337887616844d593a5f105ead8bcd
Matt Johnson-Pint <mattjohnsonpint@gmail.com>
2022-07-18 13:30:44 -0700

    Add default implementations

--- src/TimeZoneResourceProvider.Other.cs
+++ src/TimeZoneResourceProvider.Other.cs
@@ -9,11 +9,16 @@ public class TimeZoneResourceProvider : ITimeZoneResourceProvider
 
     public string GetGenericName(string timeZoneId)
     {
-        throw new NotImplementedException();
+        var tzi = TimeZoneInfo.FindSystemTimeZoneById(timeZoneId);
+        var displayName = tzi.DisplayName;
+
+        // strip off the leading "(UTC+00:00) " offset string
+        return displayName.Substring(displayName.IndexOf(')') + 1);
     }
 
     public string GetLocation(string timeZoneId)
     {
-        throw new NotImplementedException();
+        // Not available from TimeZoneInfo by default
+        return "";
     }
 }
