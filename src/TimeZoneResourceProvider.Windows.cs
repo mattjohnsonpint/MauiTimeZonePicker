d75237d4d1990a0a4ab00f1962c46d3572ae570b
Matt Johnson-Pint <mattjohnsonpint@gmail.com>
2022-07-18 13:56:32 -0700

    Get IANA IDs on Windows

--- src/TimeZoneResourceProvider.Windows.cs
+++ src/TimeZoneResourceProvider.Windows.cs
@@ -2,19 +2,39 @@ namespace MauiTimeZonePicker;
 
 public class TimeZoneResourceProvider : ITimeZoneResourceProvider, IDisposable
 {
-    public IReadOnlyList<string> GetIanaTimeZoneIds()
-    {
-        return TimeZoneInfo.GetSystemTimeZones().Select(tzi => tzi.Id).ToList();
-    }
+    public IReadOnlyList<string> GetIanaTimeZoneIds() => 
+        // TODO: This will be limited. We could P/Invoke ICU (ucal_openTimeZoneIdEnumeration) to get more data.
+        TimeZoneInfo.GetSystemTimeZones()
+            .Select(tzi =>
+            {
+                if (tzi.HasIanaId)
+                {
+                    return tzi.Id;
+                }
+
+                if (TimeZoneInfo.TryConvertWindowsIdToIanaId(tzi.Id, out var id))
+                {
+                    return id;
+                }
+
+                return "";
+            })
+            .Where(x => x != "")
+            .ToList();
 
     public string GetGenericName(string timeZoneId)
     {
-        throw new NotImplementedException();
+        var tzi = TimeZoneInfo.FindSystemTimeZoneById(timeZoneId);
+        var displayName = tzi.DisplayName;
+
+        // strip off the leading "(UTC+00:00) " offset string
+        return displayName.Substring(displayName.IndexOf(')') + 1);
     }
 
     public string GetLocation(string timeZoneId)
     {
-        throw new NotImplementedException();
+        // Not available from TimeZoneInfo by default
+        return "";
     }
 
     public void Dispose()
