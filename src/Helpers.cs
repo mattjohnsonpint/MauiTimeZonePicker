3464a3ff3e0dacb62b2174529327c421a0762492
Matt Johnson-Pint <mattjohnsonpint@gmail.com>
2022-07-18 16:59:31 -0700

    Use ICU for Windows implementation

---
+++ src/Helpers.cs
@@ -0,0 +1,18 @@
+﻿namespace MauiTimeZonePicker;
+
+internal static  class Helpers
+{
+    private static readonly string[] UtcAliases = new[] {
+        "UTC",
+        "Etc/UTC",
+        "Etc/UCT",
+        "Etc/Universal",
+        "Etc/Zulu",
+        "UCT",
+        "Universal",
+        "Zulu"
+    };
+
+    public static bool TimeZoneIsUtc(string timeZoneId) =>
+        UtcAliases.Contains(timeZoneId, StringComparer.OrdinalIgnoreCase);
+}
--- src/TimeZoneResourceProvider.Windows.cs
+++ src/TimeZoneResourceProvider.Windows.cs
@@ -1,43 +1,149 @@
+using System.Collections.Generic;
+using System.Globalization;
+using System.Runtime.InteropServices;
+
 namespace MauiTimeZonePicker;
 
-public class TimeZoneResourceProvider : ITimeZoneResourceProvider, IDisposable
+public class TimeZoneResourceProvider : ITimeZoneResourceProvider
 {
-    public IReadOnlyList<string> GetIanaTimeZoneIds() => 
-        // TODO: This will be limited. We could P/Invoke ICU (ucal_openTimeZoneIdEnumeration) to get more data.
-        TimeZoneInfo.GetSystemTimeZones()
-            .Select(tzi =>
+    private readonly DateTimeOffset _referenceDate = DateTimeOffset.UtcNow;
+    private readonly string _locale = GetLocale();
+
+    public IReadOnlyList<string> GetIanaTimeZoneIds()
+    {
+        var list = new List<string>
+        {
+            "UTC" // non-location zone
+        };
+
+        // Get location zones from ICU
+        IntPtr enumeration = default;
+        try
+        {
+            const int canonicalLocationType = 2;
+            enumeration = NativeMethods.OpenTimeZoneIDEnumeration(canonicalLocationType, null, IntPtr.Zero, out var errorCode);
+            if (errorCode > 0)
             {
-                if (tzi.HasIanaId)
+                return new string[0];
+            }
+
+            string item;
+            while ((item = NativeMethods.GetNextString(enumeration, IntPtr.Zero, out errorCode)) != null)
+            {
+                if (errorCode > 0)
                 {
-                    return tzi.Id;
+                    break;
                 }
 
-                if (TimeZoneInfo.TryConvertWindowsIdToIanaId(tzi.Id, out var id))
-                {
-                    return id;
-                }
+                list.Add(item);
+            }
 
-                return "";
-            })
-            .Where(x => x != "")
-            .ToList();
+            return list;
+        }
+        finally
+        {
+            if (enumeration != default)
+            {
+                NativeMethods.CloseEnumeration(enumeration);
+            }
+        }
+    }
 
     public string GetGenericName(string timeZoneId)
     {
-        var tzi = TimeZoneInfo.FindSystemTimeZoneById(timeZoneId);
-        var displayName = tzi.DisplayName;
-
-        // strip off the leading "(UTC+00:00) " offset string
-        return displayName.Substring(displayName.IndexOf(')') + 1);
+        var pattern = Helpers.TimeZoneIsUtc(timeZoneId) ? "zzzz" : "vvvv";
+        return GetPatternStringFromIcu(timeZoneId, pattern);
     }
 
     public string GetLocation(string timeZoneId)
     {
-        // Not available from TimeZoneInfo by default
-        return "";
+        if (Helpers.TimeZoneIsUtc(timeZoneId))
+        {
+            return "";
+        }
+
+        var location = GetPatternStringFromIcu(timeZoneId, "VVV");
+
+        // TODO: augment with region name
+
+        return location;
     }
 
-    public void Dispose()
+    private string GetPatternStringFromIcu(string timeZoneId, string pattern)
     {
+        IntPtr formatter = default;
+        try
+        {
+            const int udat_pattern = -2;
+            formatter = NativeMethods.OpenDateFormatter(udat_pattern, udat_pattern, _locale, timeZoneId, -1, pattern, -1, out var errorCode);
+            if (errorCode > 0)
+            {
+                return "";
+            }
+
+            var result = NativeMethods.FormatDate(formatter, _referenceDate, out errorCode);
+            return errorCode > 0 ? "" : result;
+        }
+        finally
+        {
+            if (formatter != default)
+            {
+                NativeMethods.CloseDateFormatter(formatter);
+            }
+        }
+    }
+
+    private static string GetLocale()
+    {
+        var locale = CultureInfo.CurrentUICulture.Name;
+        return locale.Length == 0 ? "en-US" : locale;
+    }
+
+    private class NativeMethods
+    {
+        // ucal_openTimeZoneIDEnumeration
+        // https://unicode-org.github.io/icu-docs/apidoc/dev/icu4c/ucal_8h.html#a6444141c20dfbdbedaa46b1f71dc2363
+        [DllImport("icu", EntryPoint = "ucal_openTimeZoneIDEnumeration", CharSet = CharSet.Ansi)]
+        public static extern IntPtr OpenTimeZoneIDEnumeration(int zoneType, string? region, IntPtr rawOffset, out int errorCode);
+
+        // uenum_next
+        // https://unicode-org.github.io/icu-docs/apidoc/dev/icu4c/uenum_8h.html#afc60c150cda05c0284b29d154d6486e6
+        [DllImport("icu", EntryPoint = "uenum_next", CharSet = CharSet.Ansi)]
+        public static extern string GetNextString(IntPtr enumeration, IntPtr resultLengthPtr, out int errorCode);
+
+        // uenum_close
+        // https://unicode-org.github.io/icu-docs/apidoc/dev/icu4c/uenum_8h.html#abcae42ba2a329894bcc3b37ad2a99a66
+        [DllImport("icu", EntryPoint = "uenum_close")]
+        public static extern void CloseEnumeration(IntPtr enumeration);
+
+        // udat_open
+        // https://unicode-org.github.io/icu-docs/apidoc/dev/icu4c/udat_8h.html#a4261e9fd5382197c3d86a00211c48079
+        [DllImport("icu", EntryPoint = "udat_open")]
+        public static extern IntPtr OpenDateFormatter(
+            int timeStyle,
+            int dateStyle, 
+            [MarshalAs(UnmanagedType.LPStr)] string locale,
+            [MarshalAs(UnmanagedType.LPTStr)] string timeZoneId, 
+            int timeZoneIdLength,
+            [MarshalAs(UnmanagedType.LPTStr)] string pattern,
+            int patternLength,
+            out int errorCode);
+
+        // udat_format
+        // https://unicode-org.github.io/icu-docs/apidoc/dev/icu4c/udat_8h.html#aac3747dd8edb53fc5652bdd383142bac
+        [DllImport("icu", EntryPoint = "udat_format", CharSet = CharSet.Unicode)]
+        private static extern int CallFormatDate(IntPtr formatter, long timestamp, IntPtr buffer, int bufferSize, IntPtr position, out int errorCode);
+
+        public unsafe static string FormatDate(IntPtr formatter, DateTimeOffset timestamp, out int errorCode)
+        {
+            var buffer = stackalloc char[256];
+            var length = CallFormatDate(formatter, timestamp.ToUnixTimeMilliseconds(), (IntPtr)buffer, 256, IntPtr.Zero, out errorCode);
+            return errorCode > 0 ? "" : Marshal.PtrToStringUni((IntPtr)buffer, Math.Min(length, 256));
+        }
+
+        // udat_close
+        // https://unicode-org.github.io/icu-docs/apidoc/dev/icu4c/udat_8h.html#a4cfce8b1fcf1640c4a3ada137237cb33
+        [DllImport("icu", EntryPoint = "udat_close")]
+        public static extern void CloseDateFormatter(IntPtr formatter);
     }
 }
