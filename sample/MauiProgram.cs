311152844fb8db258b6eb3f6f50a6952d446280a
Matt Johnson-Pint <matt.johnson-pint@sentry.io>
2022-07-18 22:37:00 -0700

    Add Sentry

--- sample/MauiProgram.cs
+++ sample/MauiProgram.cs
@@ -7,6 +7,11 @@ public static class MauiProgram
 		var builder = MauiApp.CreateBuilder();
 		builder
 			.UseMauiApp<App>()
+			.UseSentry(options =>
+			{
+				// This DSN is specifically for this sample application
+				options.Dsn = "https://78b4454330094076b5dbf4e064916caa@o1162446.ingest.sentry.io/6583452";
+			})
 			.ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
