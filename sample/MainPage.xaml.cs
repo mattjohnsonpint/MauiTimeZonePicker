e90a63cee65c91dca01bc18be4b8a239bd566be7
Matt Johnson-Pint <matt.johnson-pint@sentry.io>
2022-07-18 18:46:37 -0700

    Add time to UI

--- sample/MainPage.xaml.cs
+++ sample/MainPage.xaml.cs
@@ -2,9 +2,31 @@
 
 public partial class MainPage : ContentPage
 {
+	private TimeZoneResource _selectedTimeZone;
+
 	public MainPage()
 	{
 		InitializeComponent();
+	}
+
+	private void TimeZoneChanged(object sender, EventArgs e)
+	{
+		var picker = (TimeZonePicker) sender;
+		_selectedTimeZone = (TimeZoneResource) picker.SelectedItem;
+		UpdateCurrentTimeText();
+	}
+
+	private void UpdateCurrentTimeText()
+	{
+		if (_selectedTimeZone == null)
+		{
+			CurrentTimeText.IsVisible = false;
+			return;
+		}
+
+		var now = TimeZoneInfo.ConvertTime(DateTimeOffset.UtcNow, _selectedTimeZone.TimeZone);
+		CurrentTimeText.Text = $"It is {now:F} in {_selectedTimeZone.Location ?? _selectedTimeZone.Name}";
+		CurrentTimeText.IsVisible = true;
 	}
 }
 
--- src/TimeZonePicker.cs
+++ src/TimeZonePicker.cs
@@ -2,12 +2,14 @@ using System.Collections;
 
 namespace MauiTimeZonePicker;
 
+
 public class TimeZonePicker : Picker
 {
     private readonly ITimeZoneResourceProvider _resourceProvider = new TimeZoneResourceProvider();
     
     public TimeZonePicker()
     {
+        Title = "Pick a time zone...";
         ItemsSource = (IList) _resourceProvider.GetTimeZoneResources();
     }
 }
